76c53591b2b9081a975a2361890787602871b0db
Nico Schwarzer <nico.schwarzer97@gmail.com>
2026-01-29 20:52:06 +0100

    refactor [CPP_DataFrame]: using header file.

--- CPP_DataFrame.cpp
+++ CPP_DataFrame.cpp
@@ -1,177 +1,81 @@
+#include "CPP_DataFrame.h"
+#include <algorithm>  // for std::find
+#include <stdexcept>  // for exceptions
 
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <tuple>
-#include <utility>
-#include <stdexcept>
-#include <variant>
-#include <algorithm>
+// add_column
+void DataFrame::add_column(std::string col_name, std::vector<Cell>& column) {
+    columns_.push_back(col_name);
+    cols_ = columns_.size();
+    shape_.second = cols_;
 
-class DataFrame {
+    if (df_.empty()) {
+        rows_ = column.size();
+        shape_.first = rows_;
+        for (size_t i = 0; i < rows_; i++) {
+            index_.push_back(static_cast<int>(i));
+        }
+    }
+    else if (rows_ != column.size()) {
+        throw std::invalid_argument("Column size does not match existing DataFrame columns.");
+    }
 
-public:
+    df_[col_name] = column;
+}
 
-	//to allow flexibility in data type for each column
-	using Cell = std::variant<int, double, std::string, bool>;
+// set_index
+void DataFrame::set_index(std::vector<Cell>& new_index) {
+    if (new_index.size() != rows_) {
+        throw std::invalid_argument("New index size does not match number of rows in DataFrame.");
+    }
+    index_ = new_index;
+}
 
+// get_shape
+std::pair<int, int> DataFrame::get_shape() const {
+    return shape_;
+}
 
-	// No Constructed, using unordered map to store data
+// get_index
+std::vector<DataFrame::Cell> DataFrame::get_index() {
+    return index_;
+}
 
-	// Adding a column
-	// but allowing a data type - as in Pandas - so using std::string in data type declaratrion
-	// Passing column vector by reference - avoiding unnecessary copying in memory 
+// get_columns
+const std::vector<std::string>& DataFrame::get_columns() {
+    return columns_;
+}
 
-	void add_column(std::string col_name, std::vector<Cell>& column) {
+// get_column
+const std::vector<DataFrame::Cell>& DataFrame::get_column(const std::string& col_name) {
+    auto it = df_.find(col_name);
+    if (it == df_.end()) {
+        throw std::invalid_argument("Column not found in DataFrame.");
+    }
+    return it->second;
+}
 
-		columns_.push_back(col_name);
-		cols_ = columns_.size();
-		shape_.second = cols_;
+// loc
+DataFrame::Cell DataFrame::loc(const std::string col_name, size_t row) {
+    if (row >= rows_) {
+        throw std::out_of_range("Row index out of range.");
+    }
+    if (std::find(columns_.begin(), columns_.end(), col_name) == columns_.end()) {
+        throw std::out_of_range("Column Name cannot be found.");
+    }
+    return df_[col_name][row];
+}
 
-		if (df_.empty()) {
-			rows_ = column.size();
-			shape_.first = rows_;
-			// building up the index 
-			for (int i = 0; i < rows_; i++) {
-				index_.push_back(i);
-			}
-
-		}
-		else if (rows_ != column.size()) {
-			throw std::invalid_argument("Column size does not match existing DataFrame columns.");
-		}
-
-		std::vector<Cell> col_cells(column.begin(), column.end());
-		df_[col_name] = col_cells;
-
-
-	}
-
-	// Setting new index 
-	void set_index(std::vector<Cell>& new_index) {
-		if (new_index.size()  != rows_) {
-			throw std::invalid_argument("New index size does not match number of rows in DataFrame.");
-		}
-		index_ = new_index;
-	}
-
-
-	// Getter for shape
-	std::pair<int, int> get_shape() const {
-		return shape_;
-	}
-
-	// Getter for index
-	std::vector <Cell> get_index() {
-		return index_;
-	}
-
-	// Getter for columns (vector containing column names)
-	const std::vector <std::string>& get_columns()  {
-		return columns_;
-	}
-
-	// Getter for a column
-	const std::vector<Cell>& get_column(const std::string& col_name) {
-		auto it = df_.find(col_name);
-		if (it == df_.end()) {
-			throw std::invalid_argument("Column not found in DataFrame.");
-		}
-		return it -> second; // it = iterator, i.e. pointer to the pair <string, vector>
-	}
-
-
-
-
-	// LOC-alike getter function
-	Cell loc(const std::string col_name, size_t row) {
-		if (row > rows_) {
-			throw std::out_of_range("Row index out of range.");
-		}
-		else if (std::find(columns_.begin(), columns_.end(), col_name) == columns_.end()) {
-			throw std::out_of_range("Column Name cannot be found.");
-		}
-		else {
-			return df_[col_name][row];
-		}
-	}
-
-
-
-	// Modification using .loc
-	void modify_by_loc(std::string& col_name, size_t row, auto new_value) {
-		Cell new_value_cell = new_value; // converting to Cell type
-		if (row > rows_) {
-			throw std::out_of_range("Row index out of range.");
-		}
-		else if (std::find(columns_.begin(), columns_.end(), col_name) == columns_.end()) {
-			throw std::out_of_range("Column Name cannot be found.");
-		}
-		// checking if valid data type 
-		else if (new_value_cell.index() != df_[col_name][0].index()) {
-			throw std::out_of_range("Wrong data type, does not match corresponding column.");
-		} else {
-			df_[col_name][row] = new_value_cell;
-		}
-		}
-
-	// ILOC-alike getter function 
-	Cell iloc(size_t row, size_t col) {
-		if (row > rows_ && col > cols_) {
-			throw std::out_of_range("Row and column index out of range.");
-		}
-		else if (row > rows_) {
-			throw std::out_of_range("Row index out of range.");
-		}
-		else if (col > cols_) {
-			throw std::out_of_range("Column index out of range.");
-		}
-		else {
-
-		}
-		const std::string& col_name = columns_[col];
-		return df_[col_name][row];
-	}
-
-	// Modification using .iloc
-	void modify_by_iloc(size_t row, size_t col, auto new_value) {
-		Cell new_value_cell = new_value; // converting to Cell type
-		if (row > rows_) {
-			throw std::out_of_range("Row index out of range.");
-		}
-		else if (col > cols_) {
-			throw std::out_of_range("Column index out of range.");
-			// checking if valid data type 
-		} else {
-			const std::string& col_name = columns_[col];
-			if (new_value_cell.index() != df_[col_name][0].index()) {
-				throw std::out_of_range("Wrong data type, does not match corresponding column.");
-			}
-			else {
-				df_[col_name][row] = new_value_cell;
-			}
-		}
-		}
-
-	
-
-private:
-
-	// declaring df_ as unordered map structure, the backbone of the dataframe 
-	/// https://en.cppreference.com/w/cpp/container/unordered_map.html
-
-	std::unordered_map <std::string, std::vector<Cell> > df_;
-
-
-	size_t rows_ = 0;
-	size_t cols_ = 0;
-	std::vector <std::string> columns_;
-
-	std::vector <Cell> index_;
-
-	// Pandas - alike 'shape', not using tuple since its not editable, but pair 
-	std::pair  <int, int> shape_ = { 0,0 };
-
-};
-
-
+// iloc
+DataFrame::Cell DataFrame::iloc(size_t row, size_t col) {
+    if (row >= rows_ && col >= cols_) {
+        throw std::out_of_range("Row and column index out of range.");
+    }
+    if (row >= rows_) {
+        throw std::out_of_range("Row index out of range.");
+    }
+    if (col >= cols_) {
+        throw std::out_of_range("Column index out of range.");
+    }
+    const std::string& col_name = columns_[col];
+    return df_[col_name][row];
+}
---
+++ CPP_DataFrame.h
@@ -0,0 +1,78 @@
+﻿#ifndef CPP_DataFrame_H
+#define CPP_DataFrame_H
+
+#include <vector>
+#include <unordered_map>
+#include <variant>
+#include <string>
+#include <utility>
+#include <stdexcept>
+#include <algorithm>
+
+class DataFrame {
+public:
+    using Cell = std::variant<int, double, std::string, bool>;
+
+    void add_column(std::string col_name, std::vector<Cell>& column);
+    void set_index(std::vector<Cell>& new_index);
+
+    std::pair<int, int> get_shape() const;
+    std::vector<Cell> get_index();
+    const std::vector<std::string>& get_columns();
+    const std::vector<Cell>& get_column(const std::string& col_name);
+
+    Cell loc(const std::string col_name, size_t row);
+    Cell iloc(size_t row, size_t col);
+
+private:
+    std::unordered_map<std::string, std::vector<Cell>> df_;
+    size_t rows_ = 0;
+    size_t cols_ = 0;
+    std::vector<std::string> columns_;
+    std::vector<Cell> index_;
+    std::pair<int, int> shape_ = { 0, 0 };
+
+public:
+    // ⬇️ TEMPLATE METHODS MUST COME AFTER MEMBER DECLARATIONS ⬇️
+
+    template <typename T>
+    void modify_by_loc(std::string& col_name, size_t row, T new_value) {
+        Cell new_value_cell = new_value;
+
+        if (row > rows_) {
+            throw std::out_of_range("Row index out of range.");
+        }
+        else if (std::find(columns_.begin(), columns_.end(), col_name) == columns_.end()) {
+            throw std::out_of_range("Column Name cannot be found.");
+        }
+        else if (new_value_cell.index() != df_[col_name][0].index()) {
+            throw std::out_of_range("Wrong data type, does not match corresponding column.");
+        }
+        else {
+            df_[col_name][row] = new_value_cell;
+        }
+    }
+
+    template <typename T>
+    void modify_by_iloc(size_t row, size_t col, T new_value) {
+        Cell new_value_cell = new_value;
+
+        if (row > rows_) {
+            throw std::out_of_range("Row index out of range.");
+        }
+        else if (col > cols_) {
+            throw std::out_of_range("Column index out of range.");
+        }
+        else {
+            const std::string& col_name = columns_[col];
+            if (new_value_cell.index() != df_[col_name][0].index()) {
+                throw std::out_of_range("Wrong data type, does not match corresponding column.");
+            }
+            else {
+                df_[col_name][row] = new_value_cell;
+            }
+        }
+    }
+};
+
+#endif
